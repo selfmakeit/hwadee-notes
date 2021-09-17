79d9eccda59e8629af02816cd3fd34c0047bb4af
CONTAC <talktogang@gmail.com>
2021-09-17 10:01:10 +0800

    Update and rename readme to readme.java

--- 生成带logo二维码/readme.java
+++ 生成带logo二维码/readme.java
@@ -1,9 +1,13 @@
-使用方法：
+//使用方法：
 public class Main {
 	public static void main(String[] args) {
 		String logoFilePath="F:\\aa.png";
 		File logoFile = new File(logoFilePath);
+		//带图片二维码
 		File qr = QrcodeUtil.generateQRCodeWithLogo("hello", 200, logoFile, "png");
+		//不带图片二维码
+		File qr2 = QrcodeUtil.createPlainQrCode("dfdsfsdfsd", 300, "jpg");
 		System.out.println(qr.getAbsolutePath());
+		System.out.println(qr2.getAbsolutePath());
 	}
 }
