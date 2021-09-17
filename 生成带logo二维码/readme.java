538a00bcc26f086984c7b893e6f28d4b5c3e4547
CONTAC <talktogang@gmail.com>
2021-09-17 10:01:36 +0800

    Update readme.java

--- 生成带logo二维码/readme.java
+++ 生成带logo二维码/readme.java
@@ -3,9 +3,9 @@ public class Main {
 	public static void main(String[] args) {
 		String logoFilePath="F:\\aa.png";
 		File logoFile = new File(logoFilePath);
-		//带图片二维码
+		//带logo二维码
 		File qr = QrcodeUtil.generateQRCodeWithLogo("hello", 200, logoFile, "png");
-		//不带图片二维码
+		//不带logo二维码
 		File qr2 = QrcodeUtil.createPlainQrCode("dfdsfsdfsd", 300, "jpg");
 		System.out.println(qr.getAbsolutePath());
 		System.out.println(qr2.getAbsolutePath());
